4fee72e767b6f3f1a01dfd687bc18e1e2c076597
anaguernelli <anabeatrizmgp@gmail.com>
2023-09-13 19:30:25 -0300

    correção

--- estrutura_de_dados/listas/metodos/sort.py
+++ estrutura_de_dados/listas/metodos/sort.py
@@ -12,17 +12,12 @@ lista = ["Hi", "ola", "ana", "oi", "anao"]
 lista.sort(reverse=True)
 print(lista)
 
-
 # Ordenar por tamanho da palavra
 
 lista = ["Hi", "ola", "ana", "oi", "anao"]
 print(lista.sort(key=lambda x: len(x)))
-
-
 print(lista)
-
 
 lista = ["Hi", "ola", "ana", "oi", "anao"]
 print(lista.sort(key=lambda x: len(x), reverse=True))
-
 print(lista)
